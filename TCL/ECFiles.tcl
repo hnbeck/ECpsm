23fc02226bd3b7c2350f69d6e94f0e3c7106cd10
Hans Beck <beckhn@gmail.com>
2023-08-14 14:26:46 +0200

    GUI finish in first instance

--- TCL/ECFiles.tcl
+++ TCL/ECFiles.tcl
@@ -1,13 +1,23 @@
-
+###############################################################################
+#
+# TCL Script for testing and test file creation
+# Author: Hans N. Beck
+# Date: 08/2023
+# (c)
+###############################################################################
 package require fileutil
 
 # where I am?
-set currentFile  [file normalize [info script]]
-set cd [file dirname $currentFile]
+set fileBase  [file normalize [info script]]
+set cd [file dirname $fileBase]
 # source [file join $cd facts.tcl]
-set filename [file join  $cd testfile.txt]
+set ruleFile [file join  $cd rules.txt]
+set oddFile [file join  $cd odd.txt]
 
-# GUI Helper
+
+#### Helpers
+
+# GUI Helper from the book of B B Welch from 1995
 proc ScrolledListbox {parent handler args} {
     frame $parent
     eval {listbox $parent.list \
@@ -20,13 +30,58 @@ proc ScrolledListbox {parent handler args} {
     return $parent.list
 }
 
+proc ScrolledTextbox {parent args} {
+    frame $parent
+    eval {text $parent.fileText \
+            -yscrollcommand [list $parent.sy set] \
+            -xscrollcommand [list $parent.sx set] } $args
+    scrollbar $parent.sy -orient vertical -command [list $parent.fileText yview]
+    scrollbar $parent.sx -orient horizontal -command [list $parent.fileText xview]
+    pack $parent.sx -side bottom -fill x
+    pack $parent.sy -side right -fill y
+    pack $parent.fileText -side left -fill both -expand true
+    return $parent.fileText
+}
+
+
+# what to do if Indicator is selected
+proc SelectIndicator {w y} {
+    $w select set anchor [$w nearest $y]
+    set id [$w get [$w curselect]]
+    # take only the indicator part
+    set indicator [string range $id 0 1 ]
+    set effect [string range $id 4 end]
+    if {$indicator ne " "} { $::aFact insert end [string trim $indicator]}
+    FillEffects $effect
+}
+
+# what to do if Effect is selected
+proc SelectEffect {w y} {
+
+  $w select set anchor [$w nearest $y]
+  set effect [$w get [$w curselect]]
+  # combine indicator with effect
+  set indicator [string index $::fact end]
+  set index  [expr { [$::aFact index end] -1}]
+  $::aFact delete $index
+  $::aFact insert end [join [list $effect $indicator] ""]
+}
+
+# fill the Effects if Indicator is selected
+proc FillEffects {effectString} {
+    $::EList delete 0 end
+    set effectList [split $effectString " "]
+    foreach item $effectList {
+        $::EList insert end $item
+    }
+}
 
 ###### create GUI
 
 wm title . ECFiletester
 wm minsize . 800 400 
 
-
+# main frame
 frame .left -borderwidth 10 
 frame .right -borderwidth 10
 pack .left -side left -fill x -fill y
@@ -36,6 +91,8 @@ pack .right -side left -fill x
 frame .left.cmd -borderwidth 10 
 pack .left.cmd -side top -fill x
 
+# buttons
+button .left.cmd.add -text Add -command AddFact
 button .left.cmd.read -text Read -command Read
 button .left.cmd.write -text Write -command Write
 button .left.cmd.generate -text Generate -command GenPattern
@@ -44,93 +101,114 @@ pack .left.cmd.read -side left
 pack .left.cmd.write -side left
 pack .left.cmd.generate -side left
 pack .left.cmd.clear -sid left
+pack .left.cmd.add -sid left
+
+checkbutton .left.cmd.ruleOption -text Rules -variable ruleFlag
+pack .left.cmd.ruleOption -padx 10 
 
 frame .left.setup -borderwidth 10 
 pack .left.setup -side top -fill x
 
-label .left.setup.l -text File: -padx 5 -pady 5 -anchor w 
-entry .left.setup.file -width 50 -textvariable filename
-pack .left.setup.l -side top -fill x
-pack .left.setup.file -side top
+# File paths
+label .left.setup.ruleFLabel -text Rules: -padx 5 -pady 5 -anchor w 
+entry .left.setup.ruleF -width 50 -textvariable ruleFile
+pack .left.setup.ruleFLabel -side top -fill x
+pack .left.setup.ruleF -side top
 
+label .left.setup.oddFLabel -text ODD: -padx 5 -pady 5 -anchor w 
+entry .left.setup.oddF -width 50 -textvariable oddFile
+pack .left.setup.oddFLabel -side top -fill x
+pack .left.setup.oddF -side top
+
+# Indicators and effects listboxes
 label .left.setup.lIndi -text Indicators -padx 5 -pady 5 -anchor w 
-set IList [ScrolledListbox .left.setup.indicators SelectIndicator -height 5 -selectmode single]
+set IList [ScrolledListbox .left.setup.indicators SelectIndicator -height 4 -selectmode single]
 pack .left.setup.lIndi -side top -fill x
 pack .left.setup.indicators -side top -fill x
 
-
-label .left.setup.lEffects -text Domain -padx 5 -pady 5 -anchor w
-set EList [ScrolledListbox .left.setup.effects SelectEffect -height 5 -selectmode single] 
+label .left.setup.lEffects -text Effects -padx 5 -pady 5 -anchor w
+set EList [ScrolledListbox .left.setup.effects SelectEffect -height 4 -selectmode single] 
 pack .left.setup.lEffects -side top -fill x
 pack .left.setup.effects -side top -fill x
 
-label .left.setup.lFact -text Fact -padx 5 -pady 5 -anchor w
+label .left.setup.lFact -text Facts -padx 5 -pady 5 -anchor w
 set aFact [entry .left.setup.fact -width 50 -textvariable fact]
 pack .left.setup.lFact -side top -fill x
 pack .left.setup.fact -side top -fill x
 
-# right side of GUI
+bind Listbox <KeyRelease> [list SpaceBar %W %y %A]
+
+####  right side of GUI
 frame .right.t1 -borderwidth 10
-set tmp [text .right.t1.tmp -width 40 -height 8 -pady 10 -padx 10]
-pack .right.t1.tmp -side left -fill x -expand true
+label .right.t1.lRule -text {Rule input:} -padx 10 -anchor w
+set ruleInput  [ScrolledTextbox .right.t1.ruleIn  -width 40 -height 5 -pady 10 -padx 10 -wrap none]
+pack .right.t1.lRule -side top
+pack .right.t1.ruleIn -side bottom -fill x -expand true
 pack .right.t1 -side top -fill x -expand true
 
-label .right.l2 -text Output: -padx 10
-pack .right.l2 -side top
-
-## right panel
 frame .right.t2 -borderwidth 10
-set log [text .right.t2.log -width 40 -height 8 -pady 10 -padx 10]
-pack .right.t2.log -side left -fill x -expand true
+label .right.t2.lOdd -text {ODD input:} -padx 10 -anchor w
+set oddInput  [ScrolledTextbox .right.t2.oddIn  -width 40 -height 5 -pady 10 -padx 10 -wrap none]
+pack .right.t2.lOdd -side top
+pack .right.t2.oddIn -side bottom -fill x -expand true
 pack .right.t2 -side top -fill x -expand true
 
-proc SelectIndicator {w y} {
-    $w select set anchor [$w nearest $y]
-    set id [$w get [$w curselect]]
-    # take only the indicator part
-    set indicator [string range $id 0 1 ]
-    set effect [string range $id 4 end]
-    if {$indicator ne " "} { $::aFact insert end [string trim $indicator]}
-   
-    FillEffects $effect
-}
+## right panel
+frame .right.t3 -borderwidth 10
+label .right.t3.l2 -text Output: -padx 10 -anchor w
+set output [ScrolledTextbox  .right.t3.output  -width 40 -height 5 -pady 10 -padx 10 -wrap none]
+pack .right.t3.l2 -side top
+pack .right.t3.output -side top -fill x -expand true
+pack .right.t3 -side top -fill x -expand true
 
-proc SelectEffect {w y} {
-  $w select set anchor [$w nearest $y]
-  set effect [$w get [$w curselect]]
-  # combine indicator with effect
-  set indicator [lindex $::fact end]
-  set index  [expr { [$::aFact index end] -1}]
-  $::aFact delete $index
-  $::aFact insert end [join [list $effect $indicator] ""]
-}
+proc SpaceBar {w y a} {
 
-proc FillEffects {effectString} {
-
-    set effectList [split $effectString " "]
-    foreach item $effectList {
-        $::EList insert end $item
+    if {$::ruleFlag} {
+        if {$a eq " "} {
+                $::aFact insert end ">"
+        }
     }
-
+    
+   
 }
 
 proc ClearFact {} {
     $::aFact delete 0 end
 }
 
+proc AddFact {} {
+    $::output insert end $::fact\n
+    ClearFact
+}
+
 proc Write {} {
-    set chan [open $::filename  w+]
-    puts $chan [$::tmp get 1.0 end]
+    # if the fact contains a ">" its a rule
+    # an odd otherwise
+    if { [string first > $::fact 0] eq -1 } {
+        set chan [open $::oddFile  w+]
+        puts $chan $::fact
+    } else {
+        set chan [open $::ruleFile  w+]
+        puts $chan $::fact
+    }
+  
     close $chan
 }
 
 proc Read {} {
-    set chan [open $::filename  r+]
+    set chan [open $::ruleFile  r+]
     set content [read $chan]
     close $chan
-    $::log insert end $content\n
+    $::ruleInput insert end $content\n
+
+    set chan [open $::oddFile  r+]
+    set content [read $chan]
+    close $chan
+    $::oddInput insert end $content\n
 }
 
+# write an example file content (only in text box, 
+# you have to press "Write" to save it in file
 proc GenPattern {} {
 
  createFact [list {P 1} {Q 10} {A 4}]
@@ -143,21 +221,18 @@ proc createFact {indicators} {
         set $facts [dict set facts [lindex $obj 1] [lindex $obj 0] ]
     }
     foreach item [dict keys $facts] {
-        $::log insert end [join [list $item [dict get $facts $item]] ":"]
-        $::log insert end "\n"
+        $::output insert end [join [list $item [dict get $facts $item]] ":"]
+        $::output insert end "\n"
         
     }}
 
 proc createIndicators {} {
-    # set indicators [list P Q B A]
+    # Create indicator list including its domain
+    # TODO should be readable from file in future
     set effects [dict create P [list 1 2 3 4 5 6 7 8 9 10]]
     dict set effects Q [list r f a l]
     dict set effects B [list + - 0]
     dict set effects A [list r f a l]
-
-    #foreach item $indicators {
-    #    $::IList insert end $item
-    #}
 
     foreach item [dict keys $effects] {
         set domain [dict get $effects $item ]  
